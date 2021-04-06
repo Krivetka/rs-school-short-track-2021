3f688fe6dd9b91117f68c5234fb29d39413f1aad
Ivan Kuliashov <dfyreybr@gmail.com>
2021-04-07 02:15:39 +0300

    feat: add delete digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,18 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = `${n}`;
+  let str1;
+  let max = 0;
+  for (let i = 0; i < str.length; i++) {
+    str1 = '';
+    for (let j = 0; j < str.length; j++) {
+      if (i !== j) str1 += str[j];
+    }
+    if (Number.parseInt(str1, 10) > max) max = Number.parseInt(str1, 10);
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
