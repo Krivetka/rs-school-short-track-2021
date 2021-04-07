9743c40a059fdeb2e8456c68be26b5cf126ce61b
Ivan Kuliashov <dfyreybr@gmail.com>
2021-04-07 03:38:35 +0300

    feat: add file names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,25 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  let set1 = new Set(names);
+  let names1 = names;
+  while (names1.length !== set1.size) {
+    const arr = [];
+    arr.push(names1[0]);
+    let count;
+    for (let i = 1; i < names1.length; i++) {
+      count = 0;
+      for (let j = 0; j < i; j++) {
+        if (names1[i] === names1[j]) count++;
+      }
+      if (count > 0) arr.push(`${names1[i]}(${count})`);
+      else arr.push(names1[i]);
+    }
+    names1 = arr;
+    set1 = new Set(names1);
+  }
+  return names1;
 }
 
 module.exports = renameFiles;
